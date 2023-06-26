e766efa887f81621768514edf82e9371c5234b13
Roshan37 <ivzhat@gmail.com>
2023-06-26 23:06:40 +0500

    added cart screen layout

--- lib/presentation/cart.dart
+++ lib/presentation/cart.dart
@@ -12,21 +12,120 @@ class CartScreen extends StatelessWidget {
         if (state.productsList.isEmpty) {
           return const Center(child: Text('Корзина пуста'));
         } else {
-          return ListView.builder(
-            itemCount: state.productsList.length,
-            itemBuilder: (context, index) {
-              final product = state.productsList[index];
-              return ListTile(
-                title: Text(product.name),
-                trailing: IconButton(
-                  icon: const Icon(Icons.delete),
-                  onPressed: () {
-                    BlocProvider.of<CartBloc>(context)
-                        .add(RemoveFromCart(product));
+          return Column(
+            children: [
+              Expanded(
+                child: ListView.builder(
+                  padding: const EdgeInsets.fromLTRB(10.0, 15.0, 10.0, 10.0),
+                  itemCount: state.productsList.length,
+                  itemBuilder: (context, index) {
+                    final product = state.productsList[index];
+                    return Container(
+                      padding: const EdgeInsets.only(bottom: 15.0),
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Container(
+                            height: 62,
+                            width: 62,
+                            margin: const EdgeInsets.only(right: 10.0),
+                            padding: const EdgeInsets.fromLTRB(8.0, 6.0, 5.0, 3.0),
+                            decoration: const BoxDecoration(
+                              borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                              color: Color(0xFFEEEEEE),
+                            ),
+                            child: Image.network(
+                              product.image,
+                              fit: BoxFit.contain,
+                              height: 140,
+                              alignment: Alignment.center,
+                            ),
+                          ),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              LimitedBox(
+                                child: Text(
+                                  product.name,
+                                  maxLines: 2,
+                                ),
+                              ),
+                              Row(
+                                children: [
+                                  Text(
+                                    '${product.price.toString()} ₽ '
+                                  ),
+                                  const Text(
+                                    '• ',
+                                    style: TextStyle(
+                                      color: Color(0xFFBDBDBD)
+                                    ),
+                                  ),
+                                  Text(
+                                    '${product.weight.toString()}г',
+                                    style: const TextStyle(color: Colors.grey),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                          const Spacer(),
+                          Container(
+                            height: 30,
+                            decoration: const BoxDecoration(
+                              borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                              color: Color(0xFFEEEEEE),
+                            ),
+                            child: Row(
+                              children: [
+                                IconButton(
+                                  splashColor: Colors.transparent,
+                                  padding: const EdgeInsets.all(0.0),
+                                  onPressed: () {
+                                    print('remove');
+                                    // BlocProvider.of<CartBloc>(context)
+                                    //     .add(RemoveFromCart(product));
+                                  },
+                                  icon: const Icon(Icons.remove, size: 20,),
+                                ),
+                                Text('1',),
+                                IconButton(
+                                  splashColor: Colors.transparent,
+                                  padding: const EdgeInsets.all(0.0),
+                                  onPressed: () {
+                                    print('add');
+                                    // BlocProvider.of<CartBloc>(context)
+                                    //     .add(AddToCart(product));
+                                  },
+                                  icon: const Icon(Icons.add, size: 20,),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ]
+                      ),
+                    );
                   },
                 ),
-              );
-            },
+              ),
+              Padding(
+                padding: const EdgeInsets.all(15.0),
+                child: ElevatedButton(
+                  style: ButtonStyle(
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                        RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10.0),
+                        )
+                    ),
+                    fixedSize: MaterialStateProperty.all(
+                      Size(MediaQuery.of(context).size.width, 50),
+                    ),
+                  ),
+                  onPressed: () {},
+                  child: Text('Оплатить 2 004 ₽')
+                ),
+              )
+            ],
           );
         }
       },
